a21d9523e38b78f6ddb1f74886e1349076f2e421
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-03-21 20:47:22 +0600

    firebase work start

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAU_FMCyAiVg806gz9SM698FRZ91oEpgGw",
+  authDomain: "play-media-bac4f.firebaseapp.com",
+  projectId: "play-media-bac4f",
+  storageBucket: "play-media-bac4f.appspot.com",
+  messagingSenderId: "877592687939",
+  appId: "1:877592687939:web:749172908106275bac2bf6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
