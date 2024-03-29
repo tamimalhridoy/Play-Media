cbacddf38d3f6de22072deefd6ce9ea41932c2bf
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-03-29 21:39:40 +0600

    signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdClose } from "react-icons/md";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Signup() {
   let [fstName, setFstName] = useState("");
@@ -22,18 +28,48 @@ function Signup() {
       setUserError({ lastNameError: "What's Your Surname?" });
     } else if (!email) {
       setUserError({ emailError: "please Email Address you required " });
+    } else if (!password) {
+      setUserError({ passwordError: " What's Your Password?" });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          sendEmailVerification(auth.currentUser)
+          toast.success("Signup successful, Please verify your email ! ", {
+            position: "top-center",
+            autoClose: 3000,
+            closeOnClick: true,
+            theme: "light",
+          });
+          setFstName("");
+          setLastName("");
+          setEmail("");
+          setPassword("");
+        })
+        .catch((error) => {
+          console.log(error.code);
+          if (error.code.includes("auth/invalid-email")) {
+            setUserError({
+              emailError:
+                "You'll use this when you login in and if you ever need to reset your password ",
+            });
+          }
+          if (error.code.includes("auth/email-already-in-use")) {
+            setUserError({
+              emailError: "Email Alrady in used, Pleace try another email",
+            });
+          }
+          if (error.code.includes("auth/weak-password")) {
+            setUserError({
+              passwordError: "What's Your Password?",
+            });
+          }
+        });
     }
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        console.log("Signup successful!");
-      })
-      .catch((error) => {
-        console.log(error.code);
-      });
   };
 
   return (
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+      <ToastContainer />
       <div className=" container flex justify-center p-14">
         <div className="  mx-auto max-w-[432px] bg-white rounded-md shadow-lg drop-shadow-md">
           <div className="px-4 py-3 flex justify-between">
@@ -50,6 +86,7 @@ function Signup() {
             <div className="space-x-3 flex">
               <div className="">
                 <input
+                  value={fstName}
                   onChange={(e) => setFstName(e.target.value)}
                   className="flex-1 ring-1 ring-gray-400 rounded-md text-md px-2 py-2 outline-none bg-gray-100 focus:placeholder-gray-500"
                   type="text"
@@ -63,6 +100,7 @@ function Signup() {
               </div>
               <div className="">
                 <input
+                  value={lastName}
                   onChange={(e) => setLastName(e.target.value)}
                   type="text"
                   placeholder="Surname"
@@ -77,28 +115,31 @@ function Signup() {
             </div>
             <div>
               <input
+                value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full ring-1 ring-gray-400 rounded-md text-md px-2 py-2 outline-none bg-gray-100 focus:placeholder-gray-500"
                 type="text"
                 placeholder="Email address"
               />
-              {userError.emailError &&(
+              {userError.emailError && (
                 <p className="FistC bg-red-500 text-lg font-normal w-full text-white px-4 py-3 rounded-lg absolute top-[28%] left-[97%]">
-                {userError.emailError}
-              </p>
+                  {userError.emailError}
+                </p>
               )}
-              
             </div>
             <div>
               <input
+                value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full ring-1 ring-gray-400 rounded-md text-md px-2 py-2 outline-none bg-gray-100 focus:placeholder-gray-500"
                 type="password"
                 placeholder="New password"
               />
-              {/* <p className="FistD bg-red-500 text-lg font-normal text-white px-2 py-3 rounded-lg absolute top-[37%] left-[-42%]">
-                What's Your Password?
-              </p> */}
+              {userError.passwordError && (
+                <p className="FistD bg-red-500 text-lg font-normal text-white px-2 py-3 rounded-lg absolute top-[37%] left-[-42%]">
+                  {userError.passwordError}
+                </p>
+              )}
             </div>
           </div>
           <div className="px-4 py-3">
